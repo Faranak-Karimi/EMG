9b2f60f8acac1ca032113c20e22db3ef6c620b2f
Faranak <faranakkarimi.aut.ac.ir>
2020-01-01 17:07:13 +0330

    SVM updateed: Global accurecy + per user

--- Machine Learning Codes/SVM.py
+++ Machine Learning Codes/SVM.py
@@ -1,6 +1,4 @@
-# this file is intended to do the job of data manipulation
-from nltk.sentiment.util import split_train_test
-from sklearn import datasets
+# Support Vector Machine
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC, SVC
@@ -12,34 +10,42 @@ from collections import Counter
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+import os
 
-path1 = "E:/courses/Bs. Project/Classification/biopatrec-master/Dataset Features/"
-# .............data Manipulation............ #
-df = extract_table(path1)
-X = df.loc[:, 'f1ch1': 'f4ch2']  # petal length, petal width
-y = (df.loc[:, 'Category']).astype(np.float64)  # Iris-Virginica
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42, test_size=0.33)
-svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(C=1, loss="hinge")), ])
-svm_linear = svm_clf.fit(X_train, y_train)
-svm_predictions = svm_linear.predict(X_test)
-accuracy = svm_linear.score(X_test, y_test)
-cm = confusion_matrix(y_test, svm_predictions)
-print(accuracy)
+j = 3
+files = []
+li = []
+for i in range(1, 14):
+    files.append('user' + str(i) + '-' + str(j) + '-' + 'ft' + '.csv')
+for n in range(0, 13):
+    data = pd.read_csv(os.path.join('Dataset', files[n]))
+    li.append(data)
+    # .............data Manipulation............ #
+    X = data.loc[:, 'f1ch1': 'f4ch2']
+    y = (data.loc[:, 'Category']).astype(np.float64)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
+    svm_clf = Pipeline([("scaler", StandardScaler()), ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))])
+    # svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(C=16.8, loss="hinge")), ])
+    svm_linear = svm_clf.fit(X_train, y_train)
+    svm_predictions = svm_linear.predict(X_test)
+    accuracy = svm_linear.score(X_test, y_test)
+    # cm = confusion_matrix(y_test, svm_predictions)
+    print('user %s : %d '%(n, accuracy*100))
+
+y_data = (pd.concat(li, axis=0, ignore_index=True).loc[:, 'Category']).astype(np.float64)
+X_data = pd.concat(li, axis=0, ignore_index=True).loc[:, 'f1ch1': 'f4ch2']
+
+# for user-dependency test uncomment the bellow block
+
+# X_train_data = pd.concat(li, axis=0, ignore_index=True).loc[:, 'f1ch1': 'f4ch2']
+# X_test_data = pd.read_csv(os.path.join('Dataset', files[11])).loc[:, 'f1ch1': 'f4ch2']
+# y_train_data = (pd.concat(li, axis=0, ignore_index=True).loc[:, 'Category']).astype(np.float64)
+# y_test_data = (pd.read_csv(os.path.join('Dataset', files[11])).loc[:, 'Category']).astype(np.float64)
+
+X_train_data, X_test_data, y_train_data, y_test_data = train_test_split(X_data, y_data, random_state=39, test_size=0.25)
+svm_linear = svm_clf.fit(X_train_data, y_train_data)
+svm_predictions = svm_linear.predict(X_test_data)
+accuracy_data = svm_linear.score(X_test_data, y_test_data)
+print("Global Accuracy: %s" % accuracy_data)
+cm = confusion_matrix(y_test_data, svm_predictions)
 print(cm)
-# svm_model_linear = SVC(kernel='linear', C=1).fit(X_train, y_train)
-# svm_predictions = svm_model_linear.predict(X_test)
-# accuracy = svm_model_linear.score(X_test, y_test)
-# titles_options = [("Confusion matrix, without normalization", None),
-#                   ("Normalized confusion matrix", 'true')]
-# for title, normalize in titles_options:
-#     disp = plot_confusion_matrix(svm_linear, X_test, y_test,
-#                                  display_labels=df.columns.values,
-#                                  cmap=plt.cm.Blues,
-#                                  normalize=normalize)
-#     disp.ax_.set_title(title)
-#
-#     print(title)
-#     print(disp.confusion_matrix)
-#
-# plt.show()
-# creating a confusion matrix
