5858764db20591663bc5cb62467a4af9203a0dcf
Faranak karimi <faranakkarimi@aut.ac.ir>
2020-01-07 15:26:50 +0330

    dry data sets classification

--- Machine Learning Codes/SVM_1.py
+++ Machine Learning Codes/SVM_1.py
@@ -20,52 +20,88 @@ import random
 files = []
 li_train = []
 li_test = []
+svm_clf = Pipeline([("scaler", StandardScaler()), ("svm_clf", SVC(kernel="rbf", gamma=0.15, C=100))])
+# svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(loss="hinge")), ])
+
 # files.append('user' + str(1) + '-' + str(1) + '-' + 'ft' + '.csv')
 # for j in range(1, 4):
 # j = 1
-for i in range(1, 13):
-    file = 'user' + str(i) + '-' + str(1) + '-' + 'ft' + '.csv'
-    # file = 'user' + str(i) + '.csv'
-    data = pd.read_csv(os.path.join('Dataset', file))
-    # .............data Manipulation............ #
-    X = data.loc[:, 'f1ch1': 'f2ch2']
-    y = (data.loc[:, 'Category']).astype(np.float64)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
-    svm_clf = Pipeline([("scaler", StandardScaler()),("svm_clf", SVC(kernel="rbf", gamma=0.1, C=100))])
-    # svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(loss="hinge")), ])
-    svm_linear = svm_clf.fit(X_train, y_train)
-    y_pred = svm_linear.predict(X_test)
-    accuracy = svm_linear.score(X_test, y_test)
-    # randomizing DataFarme rows
-    #data = data.sample(frac=1).reset_index(drop=True)
-    li_train.append(data)
-    #li_train = random.shuffle(li_train)
-    #cm = confusion_matrix(y_test, svm_predictions)
-    print('user %s wet : %d '%(i, accuracy*100))
+for j in range(1, 4):
+    for i in range(1, 13):
+        if(j == 2 and i == 1) == 0:
+            file = 'user' + str(i) + '-' + str(j) + '-' + 'ft' + '.csv'
+            #file = 'user' + str(j) + '.csv'
+            data = pd.read_csv(os.path.join('Dataset', file))
+            # .............data Manipulation............ #
+            X = data.loc[:, 'f1ch1': 'f2ch2']
+            y = (data.loc[:, 'Category']).astype(np.float64)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
+            # svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(loss="hinge")), ])
+            svm_linear = svm_clf.fit(X_train, y_train)
+            y_pred = svm_linear.predict(X_test)
+            accuracy = svm_linear.score(X_test, y_test)
+            # randomizing DataFarme rows
+           # data = data.sample(frac=1).reset_index(drop=True)
+            li_train.append(data)
+            #li_train = random.shuffle(li_train)
+            #cm = confusion_matrix(y_test, svm_predictions)
+            print('user %s _ %f wet : %d '% (i, j, accuracy*100))
 
-
+#
+# for l in range(1, 13):
+#     file = 'user' + str(l) + '-' + str(3) + '-' + 'ft' + '.csv'
+#     # file = 'user' + str(i) + '.csv'
+#     data = pd.read_csv(os.path.join('Dataset', file))
+#     # .............data Manipulation............ #
+#     X = data.loc[:, 'f1ch1': 'f4ch2']
+#     y = (data.loc[:, 'Category']).astype(np.float64)
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
+#     # svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(loss="hinge")), ])
+#     svm_linear = svm_clf.fit(X_train, y_train)
+#     y_pred = svm_linear.predict(X_test)
+#     accuracy = svm_linear.score(X_test, y_test)
+#     # randomizing DataFarme rows
+#     #data = data.sample(frac=1).reset_index(drop=True)
+#     li_test.append(data)
+#     #li_train = random.shuffle(li_train)
+#     #cm = confusion_matrix(y_test, svm_predictions)
+#     print('user %s _ %f wet : %d '% (l, 3, accuracy*100))
 for j in range(1, 8):
-    # files.append('user' + str(i) + '-' + str(j) + '-' + 'ft' + '.csv')
-    file = 'user' + str(3) + '-' + str(j) + '-' + 'dryft'+'.csv'
-# files.append('user' + str(1) + '-' + str(3) + '-' + 'ft' + '.csv')
-# files.append('user' + str(1) + '-' + str(1) + '-' + 'ft' + '.csv')
-# for n in range(0, 13):
-#     data = pd.read_csv(os.path.join('test', file))
-    data = pd.read_csv(os.path.join(os.getcwd(), file))
-    li_test.append(data)
-    # .............data Manipulation............ #
-    X = data.loc[:, 'f1ch1': 'f2ch2']
-    y = (data.loc[:, 'Category']).astype(np.float64)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=38, test_size=0.25)
-    svm_clf = Pipeline([("scaler", StandardScaler()),("svm_clf", SVC(kernel="rbf", gamma=0.1, C=100))])
-    # svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(loss="hinge")), ])
-    svm_linear = svm_clf.fit(X_train, y_train)
-    y_pred = svm_linear.predict(X_test)
-    accuracy = svm_linear.score(X_test, y_test)
-    # cm = confusion_matrix(y_test, svm_predictions)
-    print('user3_ %s dry: %d ' % (j, accuracy*100))
+    for i in range(3, 4):
+        file = 'user' + str(i) + '-' + str(j) + '-' + 'dryft'+'.csv'
+        # data = data.sample(frac=1).reset_index(drop=True)
+        data = pd.read_csv(os.path.join('Dry Dataset', file))
+        if j < 5:
+            li_test.append(data)
+        else:
+            li_train.append(data)
+        # if(j == 1):
+            # file = 'user' + str(1) + '-' + str(3) + '-' + 'dryft' + '.csv'
+            # data = pd.read_csv(os.path.join(file))
+            # li_test.append(data)
+            # file = 'user' + str(2) + '-' + str(3) + '-' + 'dryft' + '.csv'
+            # data = pd.read_csv(os.path.join(file))
+            # li_test.append(data)
+
+        # .............data Manipulation............ #
+        X = data.loc[:, 'f1ch1': 'f2ch2']
+        y = (data.loc[:, 'Category']).astype(np.float64)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=38, test_size=0.25)
+        # svm_clf = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(loss="hinge")), ])
+        svm_linear = svm_clf.fit(X_train, y_train)
+        y_pred = svm_linear.predict(X_test)
+        accuracy = svm_linear.score(X_test, y_test)
+
+        # cm = confusion_matrix(y_test, svm_predictions)
+        print('user%g_ %s dry: %d ' % (i, j, accuracy*100))
+        svm_predictions = svm_linear.predict(X_test)
+        cm = confusion_matrix(y_test, svm_predictions)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm_d = cm.diagonal() * 100
+        print(cm_d)
+
     # randomizing DataFarme rows
-    #data = data.sample(frac=1).reset_index(drop=True)
+# data = data.sample(frac=1).reset_index(drop=True)
 #
 # for n in range(26, 38):
 #     data = pd.read_csv(os.path.join('Dataset', files[n]))
@@ -87,15 +123,15 @@ for j in range(1, 8):
 
 # for user-dependency test uncomment the bellow block
 
-# X_data = pd.concat(li_train, axis=0, ignore_index=True).loc[:, 'f1ch1': 'f4ch2']
 # li_test = random.shuffle(li_test)
 # li_train = random.shuffle(li_train)
+# X_data = pd.concat(li_train + li_test, axis=0, ignore_index=True).loc[:, 'f1ch1': 'f4ch2']
 X_test_data = pd.concat(li_test, axis=0, ignore_index=True).loc[:, 'f1ch1': 'f2ch2']
 X_train_data = pd.concat(li_train, axis=0, ignore_index=True).loc[:, 'f1ch1': 'f2ch2']
-# y_data = (pd.concat(li_train, axis=0, ignore_index=True).loc[:, 'Category']).astype(np.float64)
+# y_data = (pd.concat(li_train + li_test, axis=0, ignore_index=True).loc[:, 'Category']).astype(np.float64)
 y_test_data = (pd.concat(li_test, axis=0, ignore_index=True).loc[:, 'Category']).astype(np.float64)
 y_train_data = (pd.concat(li_train, axis=0, ignore_index=True).loc[:, 'Category']).astype(np.float64)
-# X_train_data, X_test = train_test_split(X, random_state=52, test_size=0)
+# # X_train_data, X_test = train_test_split(X, random_state=52, test_size=0)
 
 # X_train_data, X_test_data, y_train_data, y_test_data = train_test_split(X_data, y_data, random_state=39, test_size=0.25)
 
